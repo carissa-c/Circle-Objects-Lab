0493b9bfa6becf166e543b9f84ce9ccd99d4cf8e
carissa-c <122126939+carissa-c@users.noreply.github.com>
2023-01-23 19:52:03 -0600

    first upload

---
+++ Circle Objects Lab/Circle.cs
@@ -0,0 +1,47 @@
+﻿using System;
+namespace Circle_Objects_Lab
+{
+	public class Circle
+	{
+		//properties
+		public double Radius { get; set; }
+
+        //constructor
+        //a.
+        public Circle(double _radius)
+		{
+            Radius = _radius;
+		}
+
+        //methods
+        //b.
+        public double CalculateCircumference()
+        {
+            double circleCircumference = 2 * Radius * Math.PI;
+            return circleCircumference;
+        }
+        //c.
+        public string CalculateFormattedCircumference()
+        {
+            return $"Circumference: {FormatNumber(CalculateCircumference())}";
+        }
+        //d.
+        public double CalculateArea()
+        {
+            double calculatedArea = Math.PI * (Radius * Radius);
+            return calculatedArea;
+        }
+        //e.
+        public string CalculateFormattedArea()
+        {
+           return $"Area: {FormatNumber(CalculateArea())}";
+        }
+        //f.
+        public string FormatNumber(double x)
+        {
+            return Math.Round(x, 2).ToString();
+        }
+    }
+
+}
+
---
+++ Circle Objects Lab/Program.cs
@@ -0,0 +1,46 @@
+﻿using Circle_Objects_Lab;
+
+
+Console.WriteLine("Welcome to the Circle Tester!");
+
+bool runProgram = true;
+//bool response = true;
+while (runProgram)
+{
+    Console.WriteLine("Enter a radius:");
+    double userInput = double.Parse(Console.ReadLine());
+    int numOfTries = 1;
+
+    if (userInput > 0)
+    {
+        Circle myCircle = new Circle(userInput);
+        Console.WriteLine(myCircle.CalculateFormattedCircumference());
+        Console.WriteLine(myCircle.CalculateFormattedArea());
+    }
+    else if (userInput <= 0)
+    {
+        Console.WriteLine("Invalid response. Please try again.");
+        runProgram = true;
+    }
+
+    Console.WriteLine("Continue? (y/n):");
+    string response = Console.ReadLine().ToLower().Trim();
+    if (response == "y")
+    {
+        runProgram = true;
+    }
+    else if (response == "n")
+    {
+        runProgram = false;
+        Console.WriteLine($"Goodbye! You created {numOfTries} Circle Object(s)");
+
+        //foreach (Circle c in myCircle.Where()
+        //{
+        //    Console.WriteLine($"Goodbye! You created {c} Circle Object(s)");
+        //}
+    }
+    Console.ReadLine();
+}
+
+
+
